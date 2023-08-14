513f1486ba1a00f920aee52921aebb323a7cad15
Vagno silva da silva <83837716+slva643@users.noreply.github.com>
2023-08-13 22:02:55 -0300

    atualizar

---
+++ aula3/ex/index.js
@@ -0,0 +1,3 @@
+let nome = "Vagno silva da Silva"
+hora = 10
+console.log(`Meu nome é ${nome}. Estou aprendendo JavaScript às ${Number(hora)} da manhã.`)
---
+++ aula4/comentarios.js
@@ -0,0 +1,14 @@
+// E escrever um comentário longo
+console.log('Olá mundo!'); // Aqui tem outro comentário
+console.log('1');
+//comentário 
+
+/**
+ *  Um bloco de código
+ * 
+*/
+
+console.log('2'); 
+console.log('3'); 
+console.log('4'); 
+//Vai ser ignorado
