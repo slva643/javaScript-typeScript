e8aa043dfab3b4f8e8a7e9c0fa146317aa51aa98
Vagno silva da silva <83837716+slva643@users.noreply.github.com>
2023-08-17 19:17:19 -0300

    Create index.js

---
+++ aula7/index.js
@@ -0,0 +1,14 @@
+/***
+ *  Não podemos criar constantes compalavras reservadas
+ *  Constantes precisam ter nomes significativos
+ *  Não pode começar o mome de uma constantes com número
+ *  Não podem conter espaços ou traços
+ *  Utilizamos camelCase
+ *  Case-sensitive
+ *  Não podemos redeclarar constantes com let
+ *  Não utilize var, utilize const.
+*/
+
+
+
+cont
