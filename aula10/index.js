2c1f3c1d3d637a324f05ff1978fddac6037ea4bf
Vagno silva da silva <83837716+slva643@users.noreply.github.com>
2023-08-21 15:44:08 -0300

    Create index.js

---
+++ aula10/index.js
@@ -0,0 +1,57 @@
+// Dados prinitivos String, number, undefined, null, boolean, sumbol
+
+const none = 'Luiz';     // string
+const nome1 = "Luiz";    // string
+const nome2 = `Luiz`;    // string
+const num1 = 10;   // number
+const num2 = 10.52  // number
+let nomeAluno; // undefined -> não aponta para local nenhum na memória
+const sobrenomeAluno = null; // num -> não aponta para local nenhum na memória
+const aprovado = false; // Boolean = true, false (lógico)
+
+// Operadores aritméticos de atribuição e incremento
+
+/**
+ ----------------------------------
+  Aritméticos
+    + Adição / Concatenação
+    - Subtração 
+    * Multiplicação
+    / Divisão
+    ** Poteniação
+    % Resto da divisão
+ ----------------------------------
+  Ordem de precedência 
+    () 
+    ** 
+    / 
+    % 
+    +
+    - 
+ ------------------------------------  
+  Operador de decremento e incremento
+    Increnento = ++
+    Decremento = -- 
+ ------------------------------------ 
+  Operadores de Atribuição
+    +=
+    -=
+    *=
+    **=
+    /=
+ ------------------------------------ 
+  NaN -> not a number 
+  para converter uma string para um numero:
+    parseInt(),
+    parseFloat(),
+    Number(),
+
+    const num1 = 10;
+    const num2 = parseInt('5');
+    console.log(num1 + num2);
+    console.log(typeof num2)
+ ------------------------------------ 
+*/
+
+
+
