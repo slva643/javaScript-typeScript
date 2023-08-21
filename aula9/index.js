86b61d14de0829be4c066787e2677ffa394b0a31
Vagno silva da silva <83837716+slva643@users.noreply.github.com>
2023-08-21 13:19:37 -0300

    Create index.js

---
+++ aula9/index.js
@@ -0,0 +1,13 @@
+// Não usar a variável var, mas sim a variável let
+// A variável var pode ser declarado várias vez
+// Já a variável let só pode ser declaro uma vez senão ocorreira um erro.
+
+
+var nome = 'Luiz';
+var nome = 'Otávio';
+console.log(none);
+
+// Com varável let
+let nome = 'Luiz';
+nome = 'Otávio';
+console.log(nome);
