05842591ba4fb66a7ee34807d21e782e6646c3b6
Vagno silva da silva <83837716+slva643@users.noreply.github.com>
2023-08-29 20:57:05 -0300

    Create index.js

---
+++ aula14/index.js
@@ -0,0 +1,11 @@
+let num1 = 1500;
+let num2 = 2.5;
+
+console.log(num1.toString()); // O número será convertido em string
+console.log(num1.toString(2)); // pra ver o valor binário
+
+
+let num3 = 10.7552396
+console.log(num3.toFixed(2)); // Para diminuir as casas decimais
+
+console.log(Number.isInteger(num1)) // Ele vai retornar um valor boleano. true ou false
