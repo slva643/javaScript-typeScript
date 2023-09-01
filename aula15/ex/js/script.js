5b465de1e5cb273e5e032d64ae622de21c5ac210
Vagno silva da silva <83837716+slva643@users.noreply.github.com>
2023-08-31 22:01:08 -0300

    exercicio

---
+++ aula15/ex/js/script.js
@@ -0,0 +1,12 @@
+const numero = Number(prompt("Digite um número"));
+const numeroTitulo = document.getElementById('numero-titulo');
+const texto = document.getElementById('texto');
+
+numeroTitulo.innerHTML = numero;
+texto.innerHTML = '';
+texto.innerHTML += `<p>Raiz quandrada: ${Math.sqrt(numero)}</p>`;
+texto.innerHTML += `<p>${numero} é inteiro: ${Number.isSafeInteger(numero)}</p>`;
+texto.innerHTML += `<p>É NaN: ${Number.isNaN(numero)}</p>`;
+texto.innerHTML += `<p>Arredondando para baixo: ${Math.floor(numero)}</p>`;
+texto.innerHTML += `<p>Arredondando para cima: ${Math.ceil(numero)}</p>`;
+texto.innerHTML += `<p>Com duas casas decimais: ${numero.toFixed(2)}</p>`;
