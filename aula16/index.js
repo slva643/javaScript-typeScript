5ff4f87ebeb1f6ca7ba8f756669a6827ca8d3e03
Vagno silva da silva <83837716+slva643@users.noreply.github.com>
2023-09-03 22:17:39 -0300

    Create index.js

---
+++ aula16/index.js
@@ -0,0 +1,11 @@
+/*
+    Um Array no JavaScript é um objeto com um construtor único, 
+    com uma sintaxe literal e com um conjunto adicional de 
+    propriedades e de métodos herdados de um protótipo de 
+    Array. Arrays em JavaScript são usados para armazenar 
+    vários valores em uma única variável.
+
+*/
+
+const alunos = ['Luiz', 'Maria', 'João', 1, true, null];  // Funciona mais não é uma boa prática de programação
+console.log(alunos);
