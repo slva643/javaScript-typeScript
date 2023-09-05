06ffacce700479a6ae96bb3ec3039d9c89f4e900
Vagno silva da silva <83837716+slva643@users.noreply.github.com>
2023-09-04 23:09:38 -0300

    Update index.js

--- aula16/index.js
+++ aula16/index.js
@@ -7,5 +7,18 @@
 
 */
 
-const alunos = ['Luiz', 'Maria', 'João', 1, true, null];  // Funciona mais não é uma boa prática de programação
+const alunos = ['Luiz', 'Maria', 'João'];  // Funciona mais não é uma boa prática de programação
+console.log(alunos);
+
+//  Quando for preciso trocar o objeto é só endicar qual indece e agrecentar o objeto
+alunos[0] = 'Eduardo'; 
+console.log(alunos);
+
+// Também pode ser agrecentar mais valor dentro da Array. É preciso agrecentar o valor do indece e o valor do objeto
+alunos[3] = 'Luiza';
+console.log(alunos);
+
+// Também podemos usar o "lenght" para ír adionando a sequencia do indice 
+alunos [alunos.length] = 'Fabio';
+alunos [alunos.length] = 'Luana';
 console.log(alunos);
