671f0a362d433a55966c9255fc642f11be57398c
Vagno silva da silva <83837716+slva643@users.noreply.github.com>
2023-09-24 23:50:06 -0300

    Update script.js

--- aula20/js/script.js
+++ aula20/js/script.js
@@ -2,7 +2,7 @@ function meuEscopo() {
     const form = document.querySelector('.form');
     const resultado = document.querySelector('.resultado');
 
-    const pessoa = [];
+    const pessoas = [];
 
     function recebeEventoForm(evento) {
         evento.preventDefault();
@@ -11,11 +11,21 @@ function meuEscopo() {
         const sobrenome = form.querySelector('.sobrenome');
         const peso = form.querySelector('.peso');
         const altura = form.querySelector('.altura');
+
+        pessoas.push ({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value
+        });
        
-       pessoa.push(nome.value, sobrenome.value, peso.value, altura.value);
-       console.log(pessoa)
+        console.log(pessoas);
+
+
+       resultado.innerHTML += `<p>Seu nome é ${nome.value} ${sobrenome.value} você tem um peso de ${peso.value}kg e altura ${altura.value}m</p>`
     }
     form.addEventListener('submit', recebeEventoForm);
+    
 }
 
 meuEscopo();
