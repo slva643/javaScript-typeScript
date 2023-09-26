ffd891245c644d48bfc1a3a574c3e370ad402e95
Vagno silva da silva <83837716+slva643@users.noreply.github.com>
2023-09-26 16:43:57 -0300

    Create index.js

---
+++ aula21/index.js
@@ -0,0 +1,11 @@
+/**
+ * Operadores de comparação
+ * > maior que
+ * >= maior que ou igual a
+ * < menor que
+ * <= menor que ou igual a
+ * == igualdade (valor) **********
+ * === igualdade restrita (valor e tipo)
+ * != diferente (valor) ********
+ * !== diferente estrito (valor e tipo)
+*/
