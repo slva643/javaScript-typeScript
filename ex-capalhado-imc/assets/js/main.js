6706c9e8a9d7f48f7ed8b7ab65bf6262afd315bd
Vagno silva da silva <83837716+slva643@users.noreply.github.com>
2023-10-12 08:12:22 -0300

    imc

--- ex-capalhado-imc/assets/js/main.js
+++ ex-capalhado-imc/assets/js/main.js
@@ -1,18 +1,64 @@
 // Caputar evento de submit do formulário
 const form = document.querySelector('#form');
 
-form.addEventListener('submit', function (evento) {
-    evento.preventDefault();
-    console.log("Evento previnito")
-    setResultado ('Ola mundo')
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const inputPeso = e.target.querySelector('#peso');
+    const inputAltura = e.target.querySelector('#altura');
+
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+
+    if (!peso) {
+        setResultado('Peso inválido', false);
+        return;
+    }
+
+    if (!altura) {
+        setResultado('Altura inválido', false);
+        return;
+    }
+
+    const imc = getImc(peso, altura);
+    const nivelImc = getNivelImc(imc);
+    const msg = `Seu IMC é ${imc} (${nivelImc})`;
+
+    setResultado(msg, true);
 });
 
-function setResultado(msg) {
-    const resultado = document.querySelector('#resultado');
-    resultado.innerHTML = '';
-    const p = document.createElement('p');
-    p.classList.add('paragrado-resultado')
-    p.innerHTML = 'Qualquer coisa';
-    resultado.appendChild(p)
+function getNivelImc(imc) {
+    const nivel = ['Abaixo deo peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+
+    if (imc >= 39.9) return nivel[5];
+    if (imc >= 34.9) return nivel[4];
+    if (imc >= 39.9) return nivel[3];
+    if (imc >= 29.9) return nivel[2];
+    if (imc >= 18.5) return nivel[1];
+    if (imc < 18.5) return nivel[0];
 }
 
+function getImc(peso, altura) {
+    const imc = peso / altura ** 2;
+    return imc.toFixed(2);
+}
+
+function criaP() {
+    const p = document.createElement('p');
+    return p;
+}
+
+function setResultado(msg, isValid) {
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = '';
+
+    const p = criaP();
+
+    if (isValid) {
+        p.classList.add('paragrafo-resultado');
+    } else {
+        p.classList.add('bad');
+    }
+    
+    p.innerHTML = msg;
+    resultado.appendChild(p);
+}
