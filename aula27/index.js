9e8cdcbdc0569a32401fad871ccd8bf65c047042
Vagno silva da silva <83837716+slva643@users.noreply.github.com>
2023-10-16 08:38:44 -0300

    Create index.js

---
+++ aula27/index.js
@@ -0,0 +1,13 @@
+//?
+// const pontuacaoUsuario = 999;
+
+// if (pontuacaoUsuario >= 100) {
+//     console.log('Unsuário VIP');
+// } else {
+//     console.log('Usuário normal');
+// }
+
+/** Operação ternário **/
+
+const pontuacaoUSuario = 1000;
+const nivelUsuario = pontuacaoUSuario => 1000 ? 'Usuário VIP': 'Usuário normal';
