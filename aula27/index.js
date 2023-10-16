cfed03b07314320bbcd84676e64de20951866175
Vagno silva da silva <83837716+slva643@users.noreply.github.com>
2023-10-16 16:41:43 -0300

    w

--- aula27/index.js
+++ aula27/index.js
@@ -7,7 +7,19 @@
 //     console.log('Usuário normal');
 // }
 
+
+// *********************************************************************************************************************//
+
+
 /** Operação ternário **/
 
+// (condicao) ? 'valor para verdadeiro' : 'valor para falso';
+
 const pontuacaoUSuario = 1000;
-const nivelUsuario = pontuacaoUSuario => 1000 ? 'Usuário VIP': 'Usuário normal';
+const nivelUsuario = pontuacaoUSuario >= 1000 ? 'Usuário VIP': 'Usuário normal';
+
+
+const corUsuario = null;
+const corPadrao = corUsuario || 'Preta';
+
+console.log(nivelUsuario, corPadrao);
