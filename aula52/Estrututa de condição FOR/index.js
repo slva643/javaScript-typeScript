ddaac849eb5adc7ad66aa5055994fc222dba94b4
Vagno silva da silva <83837716+slva643@users.noreply.github.com>
2024-08-23 13:15:21 -0300

    Create index.js

---
+++ aula52/Estrututa de condição FOR/index.js
@@ -0,0 +1,10 @@
+// for (let i = 0; i <= 10; i++) {
+//     const par = i % 2 === 0;
+//     console.log(i, par)
+// }
+
+const frutas = ['Maça', 'Pêra', 'Uva'];
+// console.log(frutas.length)
+for (let i = 0; i < frutas.length; i++) {
+    console.log(`Indece ${i}`, frutas[i])
+}
