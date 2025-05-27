2301c724602ff2bac285c79230c8a5509694ac93
Vagno silva da silva <83837716+slva643@users.noreply.github.com>
2025-05-26 22:06:33 -0300

    Update index.js

--- aula35 - Exercício com for/index.js
+++ aula35 - Exercício com for/index.js
@@ -1,37 +1,38 @@
 
 
 const elementos = [
-  {tag: 'p', texto: 'Frase 1'}, // 0
-  {tag: 'div', texto: 'Frase 2'}, // 1
-  {tag: 'footer', texto: 'Frase 3'},
-  {tag: 'section', texto: 'Frase 3'},
+  { tag: 'p', texto: 'Frase 1' }, // 0
+  { tag: 'div', texto: 'Frase 2' }, // 1
+  { tag: 'footer', texto: 'Frase 3' },
+  { tag: 'section', texto: 'Frase 3' },
 ];
 
+/******************************************* Uma forma de resolver *******************************************************************************************************/
+// const inserir = document.querySelector('.container');
 
-const inserir = document.querySelector('.container');
+// elementos.forEach(elemento => {
+//   const novotag = document.createElement(elemento.tag)
+//   novotag.textContent = elemento.texto
+//   inserir.appendChild(novotag)
+// })
 
-elementos.forEach(elemento => {
-  const novotag = document.createElement(elemento.tag)
-  novotag.textContent = elemento.texto
-  inserir.appendChild(novotag)
-})
-
-
-
-
+/******************************************* Outra forma de resolver *******************************************************************************************************/
 
 
+const container = document.querySelector('.container');
+const div = document.createElement('div');
+
+for (let i = 0; i < elementos.length; i++) {
+  let { tag, texto } = elementos[i];
+  let tagCriada = document.createElement(tag);
+  let textoCriado = document.createTextNode(texto);
+
+  tagCriada.appendChild(textoCriado)
+  div.appendChild(tagCriada);
+}
 
 
-
-
-
-
-
-
-
-
-
+container.appendChild(div);
 
 
 // elementos.forEach(elemento => {
